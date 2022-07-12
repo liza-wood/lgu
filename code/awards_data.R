8f3599c40839588d06f96be7f7a89c45080751de
liza-wood <belwood@ucdavis.edu>
2022-07-12 12:16:41 +0100

    awards data

--- code/awards_data.R
+++ code/awards_data.R
@@ -7,13 +7,19 @@ awards <- read.csv("data_raw/university_awards/California_awards.csv",
                    skip = 2)
 breeding <- awards %>% 
   filter(Admin.Dept == "Plant Sciences") %>% 
-  filter(str_detect(Project.Title, "breed|seed|germplasm"))
+  filter(str_detect(Project.Title, "breed|seed|germplasm|variety"))
 
 breeding %>% 
   mutate(Total = as.numeric(str_remove_all(Total, "\\(|\\)|\\$|\\,"))) %>% 
   group_by(Sponsor.Type) %>% 
   summarize(Total = sum(Total, na.rm = T)) %>% 
   arrange(Total)
+
+companies <- awards %>% 
+  filter(Admin.Dept == "Plant Sciences") %>% 
+  filter(Sponsor.Type == "BUSINESS") %>% 
+  select(Sponsor) %>% 
+  unique()
 
 # Crop master list from licenses
 crops <- read.csv("crop.csv")
