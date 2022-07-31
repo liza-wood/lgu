aa90662d343c8a0ddbbfbf84caac50412278e194
liza-wood <belwood@ucdavis.edu>
2022-07-31 11:15:36 +0100

    cleaning uspto

--- code/data_collection/uspto_clean.R
+++ code/data_collection/uspto_clean.R
@@ -1,17 +1,28 @@
 library(tidyverse)
 library(lubridate)
-df <- read.csv("~/Desktop/uspto1990_2010.csv")
+setwd("~/Box/lgu")
+df <- read.csv("data_raw/other_ip/uspto.csv")
 uni_names <- read.csv("data_indices/universities.csv")
 
 # Choose only LGUs
 lgu_pattern <- paste(uni_names$university_name, collapse = "|")
+mistakes <- paste(c("University of Minnestoa", "University of Akransas", 
+                    "University of Akranas", "Texas A \\& M University",
+                    "University of Tennesse", "University of Ga", 
+                    "University of Georgiea", "North Carollna State University",
+                    "Rutger"), collapse = "|")
+lgu_pattern <- paste(lgu_pattern, mistakes, sep = "|")
+lgu_pattern <- str_replace_all(lgu_pattern, "\\bof\\b", "[Oo]f")
+
 for(i in 1:nrow(df)){
   df$university[i] <- str_extract(df$Assignee[i], lgu_pattern)
 }
 
 df <- filter(df, !is.na(university))
-df <- filter(df, !(str_detect(df$Title, "[Mm]ethod|[Aa]pparatus")) &
+df <- filter(df, !(str_detect(df$Title, "[Mm]ethod|[Aa]pparatus|^Process")) &
                !(str_detect(df$Claims, "1\\. A method")))
+
+write.csv(df, "data_clean/uspto_lgus.csv", row.names = F)
 
 # Plant patents definitely stay, but how to get utility patents
 pps <- filter(df, str_detect(df$WKU, "^PP"))
